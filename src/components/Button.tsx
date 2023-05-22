a2191fbfd555aa2fbfb463d77d77bfac4eea8559
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-05-22 14:01:38 +0300

    Update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {horizontalScale, verticalScale} from '../utils/metrics';
 
 interface ButtonProps {
   title: string;
@@ -14,7 +15,8 @@ const Button: FC<ButtonProps> = ({title, onClick}) => {
   return (
     <TouchableOpacity
       onPress={handlePress}
-      className="z-50 w-[327px] h-[56px] justify-center items-center bg-[#28AF6E] rounded-[12px] px-[18px] py-[16px]">
+      style={{...styles.container}}
+      className="z-50 justify-center items-center bg-[#28AF6E] rounded-[12px] px-[18px] py-[16px]">
       {/* //TODO: fix the text fontFamily */}
       <Text className="text-white font-rubikRegular font-[700]">{title}</Text>
     </TouchableOpacity>
@@ -22,3 +24,10 @@ const Button: FC<ButtonProps> = ({title, onClick}) => {
 };
 
 export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    width: horizontalScale(327),
+    height: verticalScale(56),
+  },
+});
