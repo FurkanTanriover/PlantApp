925445a17ea0898c4b8b86c19271c9282c256f8a
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-05-22 10:29:40 +0300

    Update navigation screens file

--- src/navigation/Screens.tsx
+++ src/navigation/Screens.tsx
@@ -1,11 +1,122 @@
+/* eslint-disable react/no-unstable-nested-components */
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Image, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {setOnboardingStatus} from '../redux/action';
 import GetStarted from '../screens/GetStarted';
+import HomeScreen from '../screens/HomeScreen';
+import Loader from '../screens/Loader';
 import Onboarding from '../screens/Onboarding';
 import PaywallScreen from '../screens/PaywallScreen';
-import HomeScreen from '../screens/HomeScreen';
-function Screens() {
-  const Stack = createNativeStackNavigator();
+import {loadOnboardingStatus} from '../utils/storage';
+import {verticalScale, horizontalScale} from '../utils/metrics';
+import DiagnoseScreen from '../screens/DiagnoseScreen';
+import MyGardenScreen from '../screens/MyGardenScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+type RootStackParamList = {
+  GetStarted: undefined;
+  Onboarding: undefined;
+  PaywallScreen: undefined;
+  TabStackScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Tab = createBottomTabNavigator();
+
+const TabStackScreen: React.FC = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color}) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = require('../assets/icons/home.png');
+          } else if (route.name === 'Diagnose') {
+            iconName = require('../assets/icons/healthcare.png');
+          } else if (route.name === 'MyGarden') {
+            iconName = require('../assets/icons/my-garden.png');
+          } else if (route.name === 'Profile') {
+            iconName = require('../assets/icons/profile.png');
+          }
+
+          return (
+            <Image
+              source={iconName}
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#28AF6E' : color,
+              }}
+              resizeMode="contain"
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        inactiveTintColor: '#979798',
+        activeTintColor: '#28AF6E',
+        keyboardHidesTabBar: true,
+        tabStyle: {},
+        style: {
+          borderTopWidth: 2,
+          borderTopColor: 'red',
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+        },
+      }}
+      sceneContainerStyle={{backgroundColor: 'white'}}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Diagnose" component={DiagnoseScreen} />
+      <Tab.Screen
+        name="Scan"
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({}) => (
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                height: verticalScale(64),
+                width: horizontalScale(64),
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('../assets/images/scan-button.png')}
+                style={{width: 60, height: 60}}
+                resizeMode="contain"
+              />
+            </View>
+          ),
+        }}
+        component={Loader}
+      />
+      <Tab.Screen name="MyGarden" component={MyGardenScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const Screens: React.FC = () => {
+  const dispatch = useDispatch();
+  const showOnboarding = useSelector(
+    (state: any) => state.reducer.showOnboarding,
+  );
+
+  useEffect(() => {
+    const checkOnboardingStatus = async () => {
+      const status = await loadOnboardingStatus();
+      if (status !== null) {
+        dispatch(setOnboardingStatus(status));
+      }
+    };
+    checkOnboardingStatus();
+  }, [dispatch]);
 
   const globalScreenOptions = {
     headerShown: false,
@@ -13,12 +124,19 @@ function Screens() {
 
   return (
     <Stack.Navigator screenOptions={globalScreenOptions}>
-      <Stack.Screen name="GetStarted" component={GetStarted} />
-      <Stack.Screen name="Onboarding" component={Onboarding} />
-      <Stack.Screen name="PaywallScreen" component={PaywallScreen} />
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      {showOnboarding ? (
+        <>
+          <Stack.Screen name="GetStarted" component={GetStarted} />
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="PaywallScreen" component={PaywallScreen} />
+          <Stack.Screen name="TabStackScreen" component={TabStackScreen} />
+        </>
+      )}
     </Stack.Navigator>
   );
-}
+};
 
 export default Screens;
