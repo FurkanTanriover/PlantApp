57fb2c95c36eac411dc6150c02b83f5f905075d6
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-05-22 14:22:05 +0300

    Create flow reset button

--- src/screens/DiagnoseScreen.tsx
+++ src/screens/DiagnoseScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {setOnboardingStatus, setPaywallStatus} from '../redux/action';
+import Button from '../components/Button';
 
 const DiagnoseScreen = () => {
+  const dispatch = useDispatch();
+
+  const handleOnboardingComplete = async () => {
+    dispatch(setOnboardingStatus(true));
+    dispatch(setPaywallStatus(true));
+  };
   return (
     <View style={styles.container}>
       <Text>Diagnose Screen</Text>
+      <Button
+        title="Onboard Flow Reset"
+        onClick={() => handleOnboardingComplete()}
+      />
     </View>
   );
 };
